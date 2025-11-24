66d444d644ac8f4ecb5ebaba9e764716868e3e6d
sophia144 <piercesp@outlook.com>
2025-11-24 13:01:41 +0000

    Began making a decision tree

--- ml_setup.py
+++ ml_setup.py
@@ -2,16 +2,19 @@
 #pip install scipy
 
 from ucimlrepo import fetch_ucirepo 
-from sklearn import tree
+from sklearn.model_selection import train_test_split
   
 # fetch dataset 
 car_evaluation = fetch_ucirepo(id=19) 
   
 # data (as pandas dataframes) 
-X = car_evaluation.data.features 
+x = car_evaluation.data.features 
 y = car_evaluation.data.targets 
-  
+
 # metadata 
 #print(car_evaluation.metadata)  
 # variable information 
 #print(car_evaluation.variables)  
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+print('done')
