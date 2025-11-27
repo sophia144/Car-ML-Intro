6aef0383087a6d17047d3301428396fb7c5be949
sophia144 <piercesp@outlook.com>
2025-11-27 14:24:35 +0000

    Cleaned data and created the decision tree

--- ml_setup.py
+++ ml_setup.py
@@ -1,20 +1,40 @@
-#pip install ucimlrepo
-#pip install scipy
-
+# imports
 from ucimlrepo import fetch_ucirepo 
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
-  
+from sklearn.preprocessing import OneHotEncoder
+
 # fetch dataset 
+# https://archive.ics.uci.edu/dataset/19/car+evaluation
 car_evaluation = fetch_ucirepo(id=19) 
   
-# data (as pandas dataframes) 
-x = car_evaluation.data.features 
-y = car_evaluation.data.targets 
+# parsing data
+x = (car_evaluation.data.features).copy()
+y = (car_evaluation.data.targets).copy()
 
-# metadata 
-#print(car_evaluation.metadata)  
-# variable information 
-#print(car_evaluation.variables)  
+# replacing strings with ints
+x.replace(
+    {'buying' : { 'vhigh' : 1, 'high' : 2, 'med' : 3, 'low' : 4 }, 
+    'maint': { 'vhigh' : 1, 'high' : 2, 'med' : 3, 'low' : 4 }, 
+    'doors' : {'2': 2, '3' : 3, '4' : 4, '5more': 5},
+    'persons' : {'2' : 2, '4' : 4, 'more': 5},
+    'lug_boot': { 'small' : 1, 'med' : 2, 'big' : 3 }, 
+    'safety': { 'low' : 1, 'med' : 2, 'high' : 3 }},
+    inplace = True
+)
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
-print('done')
+y.replace(
+    {'class' : { 'unacc' : 1, 'acc' : 2, 'good' : 3, 'vgood' : 4 }},
+    inplace = True
+)
+
+
+# splitting data into training and testing populations
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42) # 70% training and 30% testing
+
+# create Decision Tree classifer object
+clf = DecisionTreeClassifier()
+# train Decision Tree Classifer
+clf = clf.fit(x_train, y_train)
+# predict the response for test dataset
+y_pred = clf.predict(x_test)
